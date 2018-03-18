0f8f1d2e4135a6e1be58616093f99cdf7dcb7e49
Sudhanva <nsudhanva@gmail.com>
2018-03-18 16:53:53 +0530

    Download XML PDB Comments

--- download_pdb/download_async_pdb.py
+++ download_pdb/download_async_pdb.py
@@ -1,64 +1,66 @@
 # Author: (c) Sudhanva Naryana
 
+# Import Urllib, Threading, Queue, OS
 from urllib.parse import urlparse
 import urllib.request
 from threading import Thread
 import http.client, sys
 from queue import Queue
+from pathlib import Path
 import os
 
+# Check for exisiting file
 try:
     os.remove('error.txt')
 except OSError:
     pass
 
-from pathlib import Path
-
-
+# File Handler
 url_file = open('url_list.txt')
 url_read = url_file.read().replace("'", "")
-# print(type(url_read))
-
 url_list = url_read.split(",")
-# print(type(url_list))
-# print(url_list)
 
+# Number of Cuncorrent Processes
 concurrent = 500
 
+# Download the files
 def doWork():
     while True:
         url = q.get()
         status, url = getStatus(url)
-        # printStatus(status, url)
         q.task_done()
 
+# Get status of the file download
 def getStatus(ourl):
     try:
         url = urlparse(ourl)
         conn = http.client.HTTPConnection(url.netloc)   
         conn.request("HEAD", url.path)
         res = conn.getresponse()
-        # print(url.scheme + "://" + url.netloc + url.path + '.pdb')
+
+        # PDB Files
         file_name = url.path[10:] + '.pdb'
-        # print(file_name)
         request_url = url.scheme + "://" + url.netloc + url.path + '.pdb'
-        
         my_file = Path(file_name)
+
         if not my_file.is_file():
-            # file exists
+            # If file exists
             urllib.request.urlretrieve(request_url, file_name)
-            # print(file_name)
-        # print(res)
+
         return res.status, ourl
     except:
         with open('errors.txt', 'a') as error_file:
             error_file.write(str(ourl) + "\n")
         return "error", ourl
 
+# Print download status
 def printStatus(status, url):
     print(status, url)
 
+# Start Queue processing
 q = Queue(concurrent * 2)
+
+# Exit on Keyboard Interrupt
 for i in range(concurrent):
     t = Thread(target=doWork)
     t.daemon = True
