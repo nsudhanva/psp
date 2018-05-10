0e4c24a89a1245cb720c563f62c486a493dcb88a
Sudhanva <nsudhanva@gmail.com>
2018-05-10 09:57:42 +0530

    RMSD Works, not for all, still conflicts exists

--- duplicates/rmsd.py
+++ duplicates/rmsd.py
@@ -1,9 +1,12 @@
 import Bio.PDB
 from Bio.PDB.PDBParser import PDBParser
 import pandas as pd
+import numpy as np
+import warnings
 
-fragment = pd.read_excel('fragment3.xlsx')
-# writer = pd.ExcelWriter('fragment3.xlsx', engine='xlsxwriter')
+original_fragment = pd.read_excel('fragment3.xlsx')
+fragment = original_fragment
+writer = pd.ExcelWriter('fragment3_rms.xlsx', engine='xlsxwriter')
 rmsds = []
 
 for index, row in fragment.iterrows():
@@ -21,9 +24,15 @@ for index, row in fragment.iterrows():
     parser_1 = PDBParser()
     parser_2 = PDBParser()
 
-    structure_1 = parser_1.get_structure("test1", file_name_1 + '.pdb')
-    structure_2 = parser_2.get_structure("test2", file_name_2 + '.pdb')
-
+    with warnings.catch_warnings():
+        warnings.filterwarnings('error')
+        
+        try:
+            structure_1 = parser_1.get_structure("test1", file_name_1 + '.pdb')
+            structure_2 = parser_2.get_structure("test2", file_name_2 + '.pdb')
+        except Warning as e:
+            # DO nothing
+            pass
     # chains = structure_1.get_chains()
     chain_1 = structure_1[0][chain_name_1]
     chain_2 = structure_2[0][chain_name_2]
@@ -44,8 +53,8 @@ for index, row in fragment.iterrows():
     temp_1 = residues_1[start_1:end_1]
     temp_2 = residues_2[start_2:end_2]
 
-    print(temp_1)
-    print(temp_2)
+    # print(temp_1)
+    # print(temp_2)
 
     atoms_1 = []
     atoms_2 = []
@@ -60,27 +69,32 @@ for index, row in fragment.iterrows():
         # print(list(j.get_atoms()))    
         atoms_2 = atoms_2 + list(j.get_atoms())
 
-    print("Start 1:  " + str(start_1), "End 1: " + str(end_1))
-    print("Start 2:  " + str(start_2), "End 2: " + str(end_2))
+    # print("Start 1:  " + str(start_1), "End 1: " + str(end_1))
+    # print("Start 2:  " + str(start_2), "End 2: " + str(end_2))
     
-    print("Atoms 1: " + str(len(atoms_1)))
-    print("Atoms 1: " + str(list(atoms_1)))
+    # print("Atoms 1: " + str(len(atoms_1)))
+    # print("Atoms 1: " + str(list(atoms_1)))
 
-    print("Atoms 2: " + str(len(atoms_2)))
-    print("Atoms 2: " + str(list(atoms_2)))
+    # print("Atoms 2: " + str(len(atoms_2)))
+    # print("Atoms 2: " + str(list(atoms_2)))
 
     fixed = atoms_1
     moving = atoms_2
 
-    sup = Bio.PDB.Superimposer()
-    sup.set_atoms(fixed, moving)
-    print(sup.rms)
-    sup.apply(moving)
-    # print(sup.rms) 
-    rmsds.append(sup.rms)
-    # break
+    if len(atoms_1) != len(atoms_2):
+        sup = "N/A"
+        rmsds.append(sup)
+    else:
+        sup = Bio.PDB.Superimposer()
+        sup.set_atoms(fixed, moving)
+        # print(sup.rms)
+        sup.apply(moving)
+        # print(sup.rms) 
+        rmsds.append(sup.rms)
+
+#    break
 
 print(rmsds)
 fragment['RMSD'] = rmsds
-print(fragment.columns)
-# fragment.to_excel(writer)
+# print(fragment.columns)
+fragment.to_excel(writer, 'Sheet1')
