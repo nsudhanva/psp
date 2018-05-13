78cef33c22cdfc7dcaa29e1b1867ea8a7e6fc64f
Sudhanva <nsudhanva@gmail.com>
2018-05-13 20:44:24 +0530

    RMSD Seems to work, start and end error fixed, have to check for all

--- duplicates/pdb_parser.py
+++ duplicates/pdb_parser.py
@@ -3,12 +3,29 @@ from Bio.PDB import *
 
 parser_1 = PDBParser()
 parser_2 = PDBParser()
+parser_3 = PDBParser()
 structure_1 = parser_1.get_structure("test1", '102l.pdb')
 structure_2 = parser_2.get_structure("test2", '104l.pdb')
+structure_3 = parser_3.get_structure("test2", '103m.pdb')
 chain_name_1 = 'A'
 chain_name_2 = 'A'
+chain_name_3 = 'A'
 chain_1 = structure_1[0][chain_name_1]
 chain_2 = structure_2[0][chain_name_2]
+chain_3 = structure_3[0][chain_name_2]
 
-print(list(chain_1)[:5])
-print(list(chain_2)[:5])
+print(list(chain_1)[41:44])
+print(list(chain_2)[40:43])
+# print(list(chain_3))
+print(list(chain_3)[0:3])
+
+print(len(list(chain_3)))
+
+
+# hello = [1, 2, 3, 4, 5, 6, 7]
+# print(hello[0:3])
+# print(hello[1:3])
+# print(hello[3:6])
+# print(hello[3:7])
+# print(hello[4:6])
+# print(hello[4:7])
--- duplicates/rmsd.py
+++ duplicates/rmsd.py
@@ -6,21 +6,23 @@ import warnings
 warnings.filterwarnings("ignore")
 
 original_fragment = pd.read_excel('fragment3.xlsx')
-# original_fragment = original_fragment.head(50)
+
+original_fragment = original_fragment.iloc[[50]]
+# original_fragment = original_fragment.head(6)
 writer = pd.ExcelWriter('fragment3_rms.xlsx', engine='xlsxwriter')
 rmsds = []
 
-for index, row in fragment.iterrows():
+for index, row in original_fragment.iterrows():
     file_name_1 = row['ID_1'][0:4]
     file_name_2 = row['ID_2'][0:4]
     chain_name_1 = row['ID_1'][5:6]
     chain_name_2 = row['ID_2'][5:6]
 
-    start_1 = int(row['Start_1'])
-    start_2 = int(row['Start_2'])
+    start_1 = int(row['Start_1']) - 1
+    start_2 = int(row['Start_2']) - 1
 
-    end_1 = int(row['End_1']) + 1
-    end_2 = int(row['End_2']) + 1
+    end_1 = int(row['End_1'])
+    end_2 = int(row['End_2'])
 
     parser_1 = PDBParser()
     parser_2 = PDBParser()
@@ -31,23 +33,37 @@ for index, row in fragment.iterrows():
     chain_1 = structure_1[0][chain_name_1]
     chain_2 = structure_2[0][chain_name_2]
 
-    # print("Chains 1" + str(list(chain_1)))
-    # print("Chains 2" + str(list(chain_2)))
+    print("Chains 1" + str(list(chain_1)))
+    print("Chains 2" + str(list(chain_2)))
 
     residues_1 = list(chain_1)
     residues_2 = list(chain_2)
     
-    if start_1 == start_2 and end_1 == end_2:
-        start_1 = start_1 - 1
-        end_1 = end_1 - 1
-        start_2 = start_2 - 1
-        end_2 = end_2 - 1
+    residues_1 = [x for x in residues_1 if list(x.get_id())[2] == ' ']
+    residues_2 = [y for y in residues_2 if list(y.get_id())[2] == ' ']
+    
+    if list(residues_1[0].get_id())[1] == 0:
+        start_1 = start_1 + 1
+        end_1 = end_1 + 1
+    else:
+        start_1 = start_1 + list(residues_1[0].get_id())[1] - 1
+        end_1 = end_1 + list(residues_1[0].get_id())[1] - 1
+
+    if list(residues_2[0].get_id())[1] == 0:
+        start_2 = start_2 + 1
+        end_2 = end_2 + 1
+    else:
+        start_2 = start_2 + list(residues_2[0].get_id())[1] - 1
+        end_2 = end_2 + list(residues_2[0].get_id())[1] - 1
+
+    chain_count_1 = structure_1.get_chains()
+    chain_count_2 = structure_2.get_chains()
 
     temp_1 = residues_1[start_1:end_1]
     temp_2 = residues_2[start_2:end_2]
 
-    # print(temp_1)
-    # print(temp_2)
+    print(temp_1)
+    print(temp_2)
 
     atoms_1 = []
     atoms_2 = []
@@ -62,14 +78,14 @@ for index, row in fragment.iterrows():
         # print(list(j.get_atoms()))    
         atoms_2 = atoms_2 + list(j.get_atoms())
 
-    # print("Start 1:  " + str(start_1), "End 1: " + str(end_1))
-    # print("Start 2:  " + str(start_2), "End 2: " + str(end_2))
+    print("Start 1:  " + str(start_1), "End 1: " + str(end_1))
+    print("Start 2:  " + str(start_2), "End 2: " + str(end_2))
     
-    # print("Atoms 1: " + str(len(atoms_1)))
-    # print("Atoms 1: " + str(list(atoms_1)))
+    print("Atoms 1: " + str(len(atoms_1)))
+    print("Atoms 1: " + str(list(atoms_1)))
 
-    # print("Atoms 2: " + str(len(atoms_2)))
-    # print("Atoms 2: " + str(list(atoms_2)))
+    print("Atoms 2: " + str(len(atoms_2)))
+    print("Atoms 2: " + str(list(atoms_2)))
 
     fixed = atoms_1
     moving = atoms_2
