c05fe7b0bf4dfbcc8b7e37a869a68381dc542145
Sudhanva <nsudhanva@gmail.com>
2018-05-14 00:01:15 +0530

    Modified find duplicates and rmsd, they are yet to be run

---
+++ duplicates/find_duplicates.py
@@ -0,0 +1,54 @@
+# Author: (c) Sudhanva Narayana
+
+import Bio.PDB
+import numpy as np
+import pandas as pd
+import ast
+import os
+
+ROOT_DIR = os.path.dirname(os.path.abspath('__file__'))
+PARENT_DIR = os.path.abspath(os.path.join(ROOT_DIR, os.pardir))
+BROKEN_DIR = os.path.abspath(PARENT_DIR + '\\' + 'breaking')
+
+with open(BROKEN_DIR + '\\' + 'broken_files_chain.txt', 'r') as broken_chain_file:
+    file_handle = broken_chain_file.read()
+
+try:
+    file_names = ast.literal_eval(file_handle)
+except Exception as e:
+    file_names = []
+
+for frag in range(3, 42):
+    fragment_writer = pd.ExcelWriter('fragment' + str(frag) + '.xlsx')
+    print("Fragment: ", frag)
+    duplicates = []
+    result_df = pd.DataFrame()
+    test_df = pd.DataFrame()
+
+    for i in range(len(file_names)):
+        for j in range(i, len(file_names)):
+            # print(file_names[i], file_names[j])
+            pd_xlsx_1 = pd.ExcelFile(BROKEN_DIR + '\\' + file_names[i] + '.xlsx')
+            pd_xlsx_2 = pd.ExcelFile(BROKEN_DIR + '\\' + file_names[j] + '.xlsx')
+
+            pd_df_1 = pd.read_excel(pd_xlsx_1, 'fragment' + str(frag))
+            pd_df_2 = pd.read_excel(pd_xlsx_2, 'fragment' + str(frag))
+
+            pd_df_1['ID'] = pd_df_1['Metadata'][0] 
+            pd_df_2['ID'] = pd_df_2['Metadata'][0] 
+
+            pd_df_1['Resolution'] = float(pd_df_1['Metadata'][1])
+            pd_df_2['Resolution'] = float(pd_df_2['Metadata'][1])
+
+            for seq1 in range(len(pd_df_1['Fragments'])):
+                for seq2 in range(seq1 + 1 if file_names[i] == file_names[j] else 0, len(pd_df_2['Fragments'])):
+                    if (pd_df_1['Fragments'][seq1] == pd_df_2['Fragments'][seq2]):
+                        test_df = pd.DataFrame({'Fragments_1': [pd_df_1['Fragments'][seq1]], 'Start_1': [pd_df_1['Start'][seq1]], 'End_1': [pd_df_1['End'][seq1]], 'Fragments_2': [pd_df_2['Fragments'][seq2]], 'Start_2': [pd_df_2['Start'][seq2]], 'End_2': [pd_df_2['End'][seq2]], 'Resolution_1': [pd_df_1['Resolution'][0]], 'Resolution_2': [pd_df_2['Resolution'][0]], 'ID_1' : [file_names[i]], 'ID_2' : [file_names[j]], 'Atoms_1': [pd_df_1['Atom IDs'][seq1]], 'X_1': [pd_df_1['X'][seq1]], 'Y_1': [pd_df_1['Y'][seq1]], 'Z_1': [pd_df_1['Z'][seq1]], 'Atoms_2': [pd_df_2['Atom IDs'][seq2]], 'X_2': [pd_df_2['X'][seq2]], 'Y_2': [pd_df_2['Y'][seq2]], 'Z_2': [pd_df_2['Z'][seq2]]})
+                        result_df = pd.concat([result_df, test_df], ignore_index=True)
+                            
+    #        break
+    #    break
+                        
+    result_df = result_df[['ID_1', 'Fragments_1', 'Start_1', 'End_1', 'Atoms_1', 'X_1', 'Y_1', 'Z_1' , 'Resolution_1', 'ID_2', 'Fragments_2', 'Start_2', 'End_2', 'Atoms_2', 'X_2', 'Y_2', 'Z_2', 'Resolution_2']]
+    result_df.to_excel(fragment_writer)
+    fragment_writer.save()
---
+++ rmsd/rmsd.py
@@ -0,0 +1,123 @@
+import Bio.PDB
+from Bio.PDB import *
+import pandas as pd
+import numpy as np
+import ast
+import os
+import warnings
+warnings.filterwarnings("ignore")
+
+ROOT_DIR = os.path.dirname(os.path.abspath('__file__'))
+PARENT_DIR = os.path.abspath(os.path.join(ROOT_DIR, os.pardir))
+DUPLICATE_DIR = os.path.abspath(PARENT_DIR + '\\' + 'duplicates')
+PDB_DIR = os.path.abspath(PARENT_DIR + '\\' + 'download_pdb')
+
+for frag in range(3, 42):
+    original_fragment = pd.read_excel(DUPLICATE_DIR + '\\' + 'fragment' + str(frag) + '.xlsx')
+
+    # original_fragment = original_fragment.iloc[[350]]
+    # original_fragment = original_fragment.head(6)
+    writer = pd.ExcelWriter('fragment' + str(frag) + '_rms.xlsx', engine='xlsxwriter')
+    rmsds = []
+
+    for index, row in original_fragment.iterrows():
+        file_name_1 = row['ID_1'][0:4]
+        file_name_2 = row['ID_2'][0:4]
+        chain_name_1 = row['ID_1'][5:6]
+        chain_name_2 = row['ID_2'][5:6]
+
+        start_1 = int(row['Start_1']) - 1
+        start_2 = int(row['Start_2']) - 1
+
+        end_1 = int(row['End_1'])
+        end_2 = int(row['End_2'])
+
+        parser_1 = PDBParser()
+        parser_2 = PDBParser()
+
+        structure_1 = parser_1.get_structure("test1", PDB_DIR + '\\' + file_name_1 + '.pdb')
+        structure_2 = parser_2.get_structure("test2", PDB_DIR + '\\' + file_name_2 + '.pdb')
+
+        chain_1 = structure_1[0][chain_name_1]
+        chain_2 = structure_2[0][chain_name_2]
+
+        # print("Chains 1" + str(list(chain_1)))
+        # print("Chains 2" + str(list(chain_2)))
+
+        residues_1 = list(chain_1)
+        residues_2 = list(chain_2)
+        
+        residues_1 = [x for x in residues_1 if list(x.get_id())[2] == ' ']
+        residues_2 = [y for y in residues_2 if list(y.get_id())[2] == ' ']
+        
+        if list(residues_1[0].get_id())[1] == 0:
+            start_1 = start_1 + 1
+            end_1 = end_1 + 1
+        elif list(residues_1[0].get_id())[1] > 1:
+            start_1 = start_1 - list(residues_1[0].get_id())[1] + 1
+            end_1 = end_1 - list(residues_1[0].get_id())[1] + 1
+        else:
+            start_1 = start_1 + list(residues_1[0].get_id())[1] - 1
+            end_1 = end_1 + list(residues_1[0].get_id())[1] - 1
+
+        if list(residues_2[0].get_id())[1] == 0:
+            start_2 = start_2 + 1
+            end_2 = end_2 + 1
+        elif list(residues_2[0].get_id())[1] > 1:
+            start_2 = start_2 - list(residues_2[0].get_id())[1] + 1
+            end_2 = end_2 - list(residues_2[0].get_id())[1] + 1
+        else:
+            start_2 = start_2 + list(residues_2[0].get_id())[1] - 1
+            end_2 = end_2 + list(residues_2[0].get_id())[1] - 1
+
+        chain_count_1 = structure_1.get_chains()
+        chain_count_2 = structure_2.get_chains()
+
+        temp_1 = residues_1[start_1:end_1]
+        temp_2 = residues_2[start_2:end_2]
+
+        # print(temp_1)
+        # print(temp_2)
+
+        atoms_1 = []
+        atoms_2 = []
+
+        for i in temp_1:
+            # print(i.get_full_id()[3][1])
+            # print(list(i.get_atoms()))
+            atoms_1 = atoms_1 + list(i.get_atoms())
+
+        for j in temp_2:
+            # print(i.get_full_id()[3][1])
+            # print(list(j.get_atoms()))    
+            atoms_2 = atoms_2 + list(j.get_atoms())
+
+        # print("Start 1:  " + str(start_1), "End 1: " + str(end_1))
+        # print("Start 2:  " + str(start_2), "End 2: " + str(end_2))
+        
+        # print("Atoms 1: " + str(len(atoms_1)))
+        # print("Atoms 1: " + str(list(atoms_1)))
+
+        # print("Atoms 2: " + str(len(atoms_2)))
+        # print("Atoms 2: " + str(list(atoms_2)))
+
+        fixed = atoms_1
+        moving = atoms_2
+
+        if len(atoms_1) != len(atoms_2):
+            sup = "N/A"
+            rmsds.append(sup)
+        else:
+            sup = Bio.PDB.Superimposer()
+            sup.set_atoms(fixed, moving)
+            # print(sup.rms)
+            sup.apply(moving)
+            # print(sup.rms) 
+            rmsds.append(sup.rms)
+
+    #    break
+
+    # print(rmsds)
+    original_fragment['RMSD'] = pd.Series(rmsds)
+    # print(fragment.columns)
+    original_fragment.to_excel(writer, 'Sheet1')
