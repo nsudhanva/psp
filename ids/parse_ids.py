80261ec98b27744c3bd5c3526eb9d9e0e5fd3e33
Sudhanva <nsudhanva@gmail.com>
2018-05-14 17:50:23 +0530

    Original list of IDs

--- ids/parse_ids.py
+++ ids/parse_ids.py
@@ -1,5 +1,6 @@
 # Author: (c) Sudhanva Naryana
 
+import ast
 import xml.etree.ElementTree as ET
 tree = ET.parse('getCurrent.xml')
 root = tree.getroot()
@@ -12,9 +13,19 @@ for child in root:
     ids.append(child.attrib['structureId'])
     url_list.append('https://files.rcsb.org/download/' + child.attrib['structureId'])
 
-with open('id_list.txt', 'a') as id_file:
-    id_file.write(str(ids))
+print(len(ids))
 
-# Write all files to txt
-with open('url_list.txt', 'a') as url_file:
-    url_file.write(str(url_list))
+
+
+with open('protein_id_list.txt', 'r') as id_file:
+    handle = id_file.read()
+
+id_lists = ast.literal_eval(str(handle))
+print(len(id_lists))
+
+# with open('id_list.txt', 'a') as id_file:
+#     id_file.write(str(ids))
+
+# # Write all files to txt
+# with open('url_list.txt', 'a') as url_file:
+#     url_file.write(str(url_list))
